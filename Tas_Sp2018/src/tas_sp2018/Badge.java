1ce14a5e197b3ca24bf52d2185da700a923d73f2
Andrew <silverspeed132@gmail.com>
2018-03-05 12:13:38 -0600

    Why won't it work

---
+++ Tas_Sp2018/src/tas_sp2018/Badge.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tas_sp2018;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Badge {
+           private String description;
+	   private String id;
+	
+	   public Badge(){
+		description = "";
+		id = "";
+	}
+	   public Badge(String description, String id){
+		this.description = description;
+		this.id = id;
+	}
+	
+	   public String getDescription() {return description;}
+	   public String getId() {return id;}
+
+
+	   public void setDescription(String description) { this.description = description;}
+	   public void setId(String id) { this.id = id;}
+
+
+	   @Override
+	   public String toString(){
+		return "#" + getId() + " (" + getDescription() + ")";
+	}
+    }
+
